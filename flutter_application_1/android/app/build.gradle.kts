da0ba0ab0b7a49f2d27ce6c244be8f89e65bb96f
yousef <youssef_nabil2006@outlook.com>
2025-07-11 16:42:07 +0300

    fix

--- flutter_application_1/android/app/build.gradle.kts
+++ flutter_application_1/android/app/build.gradle.kts
@@ -23,7 +23,8 @@ dependencies {
 android {
     namespace = "com.example.flutter_application_1"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
 
 
     compileOptions {
